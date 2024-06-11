28f74e0defbec3a1e7354a39fa14a5cd3018a9d4
Sualp Danacı <sualpdnc@gmail.com>
2024-06-11 17:52:12 +0300

    Initial Commit

---
+++ Coffe Guide/Coffe_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffe_GuideApp.swift
+//  Coffe Guide
+//
+//  Created by Sualp Danacı on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Coffe_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
