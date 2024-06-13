fb09e7fbd9dffdf62d4f77c8ecda85f336d2541d
Sualp Danacı <sualpdnc@gmail.com>
2024-06-13 23:16:36 +0300

    initial commit

---
+++ Coffe Guide/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Coffe Guide
+//
+//  Created by Sualp Danacı on 13.06.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile on progress")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
